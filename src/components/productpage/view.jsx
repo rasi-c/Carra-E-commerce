7566c0d5cfd7c42193e4e13a04bb55f5fcc0a518
sahlct <mhdsahlct@gmail.com>
2024-03-11 11:03:56 +0530

    just commit , no updates

--- src/components/productpage/view.jsx
+++ src/components/productpage/view.jsx
@@ -3,25 +3,37 @@ import { useParams } from "react-router-dom";
 import "./view.css"
 
 export function View() {
-  const data = useParams();
-  //   console.log("data is ",data);
-  const [singleData, setsingleData] = useState({});
+  const { cardId } = useParams();
+  const [singleData, setSingleData] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`https://fakestoreapi.com/products/${data.cardId}`)
+    fetch(`https://fakestoreapi.com/products/${cardId}`)
       .then((res) => res.json())
-      .then((singleData) => setsingleData(singleData));
-  }, []);
+      .then((data) => {
+        setSingleData(data);
+        setLoading(false); // Set loading to false after data is fetched
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, [cardId]);
+
   return (
-    <div className="containerDetails">
-      <div className="header1">{singleData?.title}</div>
-      <img src={singleData?.image} alt="i" className="photo1" />
-      <div className="details1">{singleData?.description}</div>
-      <div className="count1">
-        <div className="catogory1">Catogory : {singleData?.category}</div>
-        <div className="rating1">Rating : {singleData?.rating?.rate}</div>
-        <div className="sale1">Solded Count : {singleData?.rating?.count}</div>
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <div className="spinner"></div>
+      ) : (
+        <div className="containerDetails">
+          <div className="header1">{singleData?.title}</div>
+          <img src={singleData?.image} alt="i" className="photo1" />
+          <div className="details1">{singleData?.description}</div>
+          <div className="count1">
+            <div className="catogory1">Category : {singleData?.category}</div>
+            <div className="rating1">Rating : {singleData?.rating?.rate}</div>
+            <div className="sale1">Sold Count : {singleData?.rating?.count}</div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
+
